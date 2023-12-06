d5153d47de02108035e9bf66af4020cce173db6c
willie prakasa <127827978+willieprakasa22@users.noreply.github.com>
2023-12-06 17:44:38 +0700

    Add files via upload

---
+++ PerpustakaanAppMVC/PerpustakaanAppMVC/Model/Context/DbContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Data.SQLite;
+
+namespace PerpustakaanAppMVC.Model.Context
+{
+    public class DbContext : IDisposable
+    {
+        // deklarasi private variabel / field
+        private SQLiteConnection _conn;
+
+        // deklarasi property Conn (connection), untuk menyimpan objek koneksi
+        public SQLiteConnection Conn
+        {
+            get { return _conn ?? (_conn = GetOpenConnection()); }
+        }
+
+        // Method untuk melakukan koneksi ke database
+        private SQLiteConnection GetOpenConnection()
+        {
+            SQLiteConnection conn = null; // deklarasi objek connection
+
+            try // penggunaan blok try-catch untuk penanganan error
+            {
+                // cara mengeset lokasi database
+
+                // 1. set lokasi database secara absolute
+                //string dbName = @"D:\Database\DbPerpustakaan.db";
+
+                // 2. set lokasi database secara relative
+                string dbName = Directory.GetCurrentDirectory() + "\\Database\\DbPerpustakaan.db";
+
+                // deklarasi variabel connectionString, ref: https://www.connectionstrings.com/
+                string connectionString = string.Format("Data Source={0};FailIfMissing=True", dbName);
+
+                conn = new SQLiteConnection(connectionString); // buat objek connection
+                conn.Open(); // buka koneksi ke database
+            }
+            // jika terjadi error di blok try, akan ditangani langsung oleh blok catch
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print("Open Connection Error: {0}", ex.Message);
+            }
+
+            return conn;
+        }
+
+        // Method ini digunakan untuk menghapus objek koneksi dari memory ketika sudah tidak digunakan
+        public void Dispose()
+        {
+            if (_conn != null)
+            {
+                try
+                {
+                    if (_conn.State != ConnectionState.Closed) _conn.Close();
+                }
+                finally
+                {
+                    _conn.Dispose();
+                }
+            }
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ PerpustakaanAppMVC/PerpustakaanAppMVC/Model/Repository/MahasiswaRestApiRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using PerpustakaanAppMVC.Model.Entity;
+
+namespace PerpustakaanAppMVC.Model.Repository
+{
+    public class MahasiswaRestApiRepository
+    {
+		public int Create(Mahasiswa mhs)
+		{
+			string baseUrl = "http://latihan.coding4ever.net:5555/";
+			string endpoint = "api/mahasiswa";
+
+			// membuat objek rest client
+			var client = new RestClient(baseUrl);
+
+			// membuat objek request
+			var request = new RestRequest(endpoint, Method.POST);
+
+			request.AddJsonBody(mhs);
+
+			// kirim request ke server
+			var response = client.Execute(request);
+			var result = response.Content.IndexOf("1") > 0 ? 1 : 0;
+
+			return result;
+		}
+
+		public int Update(Mahasiswa mhs)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Delete(Mahasiswa mhs)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Mahasiswa> ReadAll()
+		{
+			string baseUrl = "http://latihan.coding4ever.net:5555/";
+			string endpoint = "api/mahasiswa";
+
+			// membuat objek rest client
+			var client = new RestClient(baseUrl);
+
+			// membuat objek request
+			var request = new RestRequest(endpoint, Method.GET);
+
+			// kirim request ke server
+			var response = client.Execute<List<Mahasiswa>>(request);
+
+			return response.Data;
+		}
+
+		public List<Mahasiswa> ReadByNama(string nama)
+		{
+			string baseUrl = "http://latihan.coding4ever.net:5555/";
+			string endpoint = "api/mahasiswa?nama="+nama;
+
+			// membuat objek rest client
+			var client = new RestClient(baseUrl);
+
+			// membuat objek request
+			var request = new RestRequest(endpoint, Method.GET);
+
+			// kirim request ke server
+			var response = client.Execute<List<Mahasiswa>>(request);
+
+			return response.Data;
+		}
+
+		public Mahasiswa ReadByNpm(string npm)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
